f0c8d6db29ef2237570c315ff954abd6fe89a4e1
Erwan Normand <normand.erwan@protonmail.com>
2018-10-17 20:27:13 +0200

    Fix execute.bat

--- execute.bat
+++ execute.bat
@@ -1,2 +1,2 @@
-"%UserProfile%\Miniconda3\python.exe" "%UserProfile%\Miniconda3\cwp.py" "%UserProfile%\Miniconda3\envs\master-thesis" "%UserProfile%\Miniconda3\envs\master-thesis\python.exe" "%UserProfile%\Miniconda3\envs\master-thesis\Scripts\jupyter-notebook-script.py" "%~dp0Handheld VESAD Analysis.ipynb"
+"%UserProfile%\Miniconda3\python.exe" "%UserProfile%\Miniconda3\cwp.py" "%UserProfile%\Miniconda3\envs\handheld-vesad" "%UserProfile%\Miniconda3\envs\handheld-vesad\python.exe" "%UserProfile%\Miniconda3\envs\handheld-vesad\Scripts\jupyter-notebook-script.py" "%~dp0Handheld VESAD Analysis.ipynb"
 pause
